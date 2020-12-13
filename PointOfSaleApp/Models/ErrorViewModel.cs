4472d6cdad9ce3415f210ab714d9519f58ffc902
Abrarulctg <jubarictg99@gmail.com>
2020-12-13 18:47:23 +0600

    First Commit-abrarul

---
+++ PointOfSaleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PointOfSaleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
